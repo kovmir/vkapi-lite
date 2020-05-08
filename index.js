c4432da02aea98aaff009524b399f16e65e2488e
unInstance <uninstance@protonmail.com>
2020-05-08 14:01:27 +0200

    Migrate from request to bent

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const request = require('request-promise-native');
 const EventEmitter = require('events');
+const bent = require('bent');
 
-class UserInstance extends EventEmitter {
+// Extends EventEmitter to emit events from the long polling server.
+class ClientInstance extends EventEmitter {
   constructor(options = {}) {
-    super();
+    super(); // Call EventEmitter's constructor.
 
-    if (typeof options !== 'object' || Array.isArray(options) || options === null) {
+    // typeof operator things arrays and nulls are objects as well.
+    if (typeof options !== 'object' || Array.isArray(options) ||
+        options === null) {
       throw new Error('Invalid options type! Required type: Object');
     }
 
@@ -15,138 +18,131 @@ class UserInstance extends EventEmitter {
       }
     }
 
+    // PRIVATE
+    this._getJSON = bent('json');
+    this._APIAddress = 'https://api.vk.com/method/';
+    this._botPoll = false;
+
+    // PUBLIC
     this.token = options.token || '';
-    this.version = options.version || '5.68';
-    this.logger = options.logger;
-    this.debug = options.debug;
+    this.version = options.version || '5.125';
+    this.logger = options.logger || console.log;
+    this.debug = options.debug || false;
   }
 
-  callMethod(methodName, params = {}) {
+  // PRIVATE METHODS
+
+  // bent library does not allow you to pass URL params as an object, so I use
+  // this function to construct a string of the params from the object.
+  _buildURLParams(prefix, params) {
+    let URLParams = prefix + '?';
+    for (let key in params) {
+      URLParams += key;
+      URLParams += '='
+      URLParams += params[key];
+      URLParams += '&';
+    }
+    // Get rid of '&' at the end.
+    return URLParams.substring(0, URLParams.length-1);
+  }
+
+  // Fetches updates from the polling server.
+  async _getUpdates (lpsAddress, lpsParams, params) {
+    this.debug && this.logger('[lp request]', lpsAddress, lpsParams);
+
+    const reqURL = `${lpsAddress}${this._buildURLParams('', lpsParams)}`;
+    let respObj;
+    try {
+      respObj = await this._getJSON(reqURL);
+    } catch (err) {
+      this.debug && this.logger('[lp error]', err);
+      this.startBotPolling(params);
+    }
+
+    this.debug && this.logger('[lp response]', lpsAddress, respObj);
+
+    if (respObj.updates) {
+      // Possible update types: https://vk.com/dev/groups_events
+      for (const update of respObj.updates) {
+        this.emit(update.type, update);
+      }
+
+      if (this._botPoll) {
+        lpsParams.ts = respObj.ts;
+        this._getUpdates(lpsAddress, lpsParams, params);
+      }
+      return;
+    }
+
+    // Possible errors: https://vk.com/dev/bots_longpoll?f=2.2.%20%D0%9E%D1%88%D0%B8%D0%B1%D0%BA%D0%B8
+    if (respObj.failed) {
+      switch (respObj.failed) {
+        case 1:
+          lpsParams.ts = respObj.ts;
+          this._getUpdates(lpsAddress, lpsParams, params);
+          break;
+        case 2: case 3:
+          this.startBotPolling(params);
+          break;
+      }
+    }
+    // VK API is so horrible that it can return anything: an empty string, an
+    // invalid JSON, etc. Thus I put this statement here to prevent sudden
+    // failures with no reason.
+    this.startBotPolling(params);
+  };
+
+  // PUBLIC METHODS
+
+  // Use this to call methonds.
+  async callMethod(methodName, params = {}) {
     if (typeof methodName !== 'string') {
       throw new Error('Invalid methodName type! Required type: String');
     }
 
-    this.debug && this.logger && this.logger('[call method]', methodName, params);
+    // typeof operator things arrays and nulls are objects as well.
+    if (typeof params !== 'object' || Array.isArray(params) ||
+        params === null) {
+      throw new Error('Invalid options type! Required type: Object');
+    }
+
+    this.debug && this.logger('[call method]', methodName, params);
 
     params.access_token = params.access_token || this.token;
     params.v = params.v || this.version;
 
-    return request({
-      uri: `https://api.vk.com/method/${methodName}`,
-      qs: params,
-      json: true
-    }).then((response) => {
-      this.debug && this.logger && this.logger('[method response]', methodName, response);
-
-      if (response.response) {
-        return Promise.resolve(response.response);
-      }
-      
-      if (response.error) {
-        return Promise.reject(new Error(response.error.error_msg));
-      }
-
-      return Promise.resolve(response);
-    });
+    const reqURL = `${this._APIAddress}${this._buildURLParams(methodName, params)}`;
+    const respObj = await this._getJSON(reqURL);
+    return respObj.response || respObj.error;
   }
 
-  startPolling(params = {}) {
-    if (typeof params !== 'object' || Array.isArray(params) || params === null) {
+  // Starts Bots Long Poll.
+  // https://vk.com/dev/bots_longpoll
+  async startBotPolling(params = {}) {
+    if (typeof params !== 'object' || Array.isArray(params) ||
+        params === null) {
       throw new Error('Invalid params type! Required type: Object');
     }
 
-    const event = new Map([
-      [1, 'replaceMessageFlags'],
-      [2, 'installMessageFlags'],
-      [3, 'resetMessageFlags'],
-      [4, 'newMessage'],
-      [6, 'readAllIncomingMessages'],
-      [7, 'readAllOutgoingMessages'],
-      [8, 'friendIsOnline'],
-      [9, 'friendIsOffline'],
-      [10, 'resetDialogFlags'],
-      [11, 'replaceDialogFlags'],
-      [12, 'installDialogFlags'],
-      [13, 'deleteAllMessages'],
-      [51, 'conversationContentOrTopicChange'],
-      [61, 'userTypingInDialog'],
-      [62, 'userTypingInConversation'],
-      [70, 'userCompleteCall'],
-      [80, 'newUnreadMessageCounter'],
-      [114, 'notificationSettingsChange']
-    ]);
-
-    const getUpdates = (lpsAddress, lpsParams) => {
-      this.debug && this.logger && this.logger('[lp request]', lpsAddress, lpsParams);
-
-      request({
-        uri: lpsAddress,
-        qs: lpsParams,
-        json: true
-      }).then((response) => {
-        this.debug && this.logger && this.logger('[lp response]', lpsAddress, response);
-
-        if (response.updates) {
-          for (const update of response.updates) {
-            this.emit(event.get(update[0]), update);
-          }
-
-          if (this._polling) {
-            lpsParams.ts = response.ts;
-            getUpdates(lpsAddress, lpsParams);
-          }
-
-          return;
-        }
-
-        if (response.failed) {
-          switch (response.failed) {
-            case 1:
-              lpsParams.ts = response.ts;
-              getUpdates(lpsAddress, lpsParams);
-              break;
-            case 2: case 3:
-              this.startPolling(params);
-              break;
-            case 4:
-              lpsParams.version = response.max_version;
-              getUpdates(lpsAddress, lpsParams);
-              break;
-          }
-
-          return;
-        }
-
-        this.startPolling(params);
-      }).catch(() => {
-        this.startPolling(params);
-      });
+    const lps = await this.callMethod('groups.getLongPollServer', {
+      group_id: 195124061,
+    });
+    const lpsAddress = lps.server;
+    const lpsParams = {
+      act: 'a_check',
+      key: lps.key,
+      ts: lps.ts,
+      wait: params.wait || 25,
     };
 
-    this.callMethod('messages.getLongPollServer', {
-      need_pts: 0,
-      lp_version: params.lp_version || 2
-    }).then((lps) => {
-      const lpsAddress = `https://${lps.server}`;
-      const lpsParams = {
-        act: 'a_check',
-        key: lps.key,
-        ts: lps.ts,
-        wait: params.wait || 25,
-        mode: params.mode || 2,
-        version: params.version || 2
-      };
-
-      this._polling = true;
-
-      getUpdates(lpsAddress, lpsParams);
-    });
+    this._botPoll = true;
+    this._getUpdates(lpsAddress, lpsParams, params);
   }
 
-  stopPolling() {
-    this._polling = false;
+  // Waits unitl the last request gets fulfilled and stops Bots Long Poll.
+  stopBotPolling() {
+    this._botPoll = false;
   }
 }
 
-module.exports = UserInstance;
-
+module.exports = ClientInstance;
